543e92241610d9cd017c910e9d38b7f903fcd4ac
juanchii-coder <71896282+juanchii-coder@users.noreply.github.com>
2024-03-28 20:00:26 -0300

    nashe2.0

--- 2024/.NET/Practica/proyectosDotnet/teoriaMonte/Program.cs
+++ 2024/.NET/Practica/proyectosDotnet/teoriaMonte/Program.cs
@@ -1,1 +1,134 @@
-﻿
+﻿//punto 5
+/*
+double[,] matriz = new double[,]
+{{1,4,7},
+{3,2,9},
+{5,6,8}};
+double[][] x;
+x = GetArregloDeArreglo(matriz);
+imprimir(x);
+
+double[][] GetArregloDeArreglo(double[,] matriz)
+{
+  double[][] vv = new double[matriz.GetLength(0)][];
+  for (int j = 0; j < matriz.GetLength(0); j++)
+  {
+    vv[j] = new double[matriz.GetLength(0)];
+  }
+  for (int i = 0; i < (matriz.GetLength(0) * matriz.GetLength(1)); i++)
+  {
+    vv[i / matriz.GetLength(1)][i % matriz.GetLength(1)] = matriz[i / matriz.GetLength(1), i % matriz.GetLength(1)];
+    Console.WriteLine(" / " + i / matriz.GetLength(1) + " y % " + i % matriz.GetLength(1));
+  }
+  return vv;
+}
+void imprimir(double[][] vv)
+{
+  foreach (double[] v in vv)
+  {
+    foreach (double num in v)
+    {
+      Console.WriteLine(num);
+    }
+  }
+}
+*/
+
+//punto 6
+
+double[,] A = new double[,]
+{ { 1, 1 },
+{ 2, 2 },
+{ 3, 3 } };
+
+double[,] B = new double[,]
+{ { 1, 1 },
+{ 2, 2 },
+{ 3, 3 } };
+
+double[,] C = new double[,]
+{ { 1, 1, 1 },
+{ 2, 2, 2 } };
+
+double[,]? suma = Suma(A, B);
+double[,]? resta = Resta(A, B);
+double[,] multi = Multiplicacion(A, C);
+
+Console.WriteLine("matriz A:");
+imprimir(A);
+
+Console.WriteLine("matriz B:");
+imprimir(B);
+
+Console.WriteLine("matriz C:");
+imprimir(C);
+
+Console.WriteLine("matriz suma:");
+imprimir(suma);
+
+Console.WriteLine("matriz resta:");
+imprimir(resta);
+
+Console.WriteLine("matriz multiplicacion:");
+imprimir(multi);
+
+double[,]? Suma(double[,] A, double[,] B)
+{
+  double[,] suma = new double[A.GetLength(0), A.GetLength(1)];
+  if ((A.GetLength(0) == B.GetLength(0)) & (A.GetLength(1) == B.GetLength(1)))
+  {
+    for (int i = 0; i < (A.GetLength(0) * A.GetLength(1)); i++)
+    {
+      suma[i / A.GetLength(1), i % A.GetLength(1)] = A[i / A.GetLength(1), i % A.GetLength(1)] + B[i / A.GetLength(1), i % A.GetLength(1)];
+    }
+  }
+  return suma;
+}
+double[,]? Resta(double[,] A, double[,] B)
+{
+  double[,] resta = new double[A.GetLength(0), A.GetLength(1)];
+  if ((A.GetLength(0) == B.GetLength(0)) & (A.GetLength(1) == B.GetLength(1)))
+  {
+    for (int i = 0; i < (A.GetLength(0) * A.GetLength(1)); i++)
+    {
+      resta[i / A.GetLength(1), i % A.GetLength(1)] = A[i / A.GetLength(1), i % A.GetLength(1)] - B[i / A.GetLength(1), i % A.GetLength(1)];
+    }
+  }
+  return resta;
+}
+double[,] Multiplicacion(double[,] A, double[,] B)
+{
+  double[,] multi = new double[A.GetLength(0), A.GetLength(1)];
+  if ((A.GetLength(0) == B.GetLength(1)) & (A.GetLength(1) == B.GetLength(0)))
+  {
+    for (int i = 0; i < (A.GetLength(0) * A.GetLength(1)); i++)
+    {
+      multi[i / A.GetLength(1), i % A.GetLength(1)] = A[i / A.GetLength(1), i % A.GetLength(1)] * B[i % A.GetLength(1), i / A.GetLength(1)];
+    }
+  }
+  else
+  {
+    throw new ArgumentException("no se puede hacer la multiplicacion");
+  }
+  return multi;
+}
+void imprimir(double[,] v)
+{
+  if (v != null)
+  {
+    for (int i = 0; i < v.GetLength(0); i++)
+    {
+
+      for (int j = 0; j < v.GetLength(1); j++)
+      {
+        Console.Write($"| {v[i, j]} |");
+      }
+      Console.WriteLine();
+      Console.WriteLine("------------");
+    }
+  }
+  else
+  {
+    Console.WriteLine("no hay nada en el vector");
+  }
+}
--- 2024/.NET/Practica/proyectosDotnet/teoriaMonte/obj/Debug/net8.0/teoriaMonte.AssemblyInfo.cs
+++ 2024/.NET/Practica/proyectosDotnet/teoriaMonte/obj/Debug/net8.0/teoriaMonte.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("teoriaMonte")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fdb04f2dfdabd33e975c2eea4aa29715ba8da7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cfa18eb47af46d7e1fc1e8bc5c751678cb4d838")]
 [assembly: System.Reflection.AssemblyProductAttribute("teoriaMonte")]
 [assembly: System.Reflection.AssemblyTitleAttribute("teoriaMonte")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
