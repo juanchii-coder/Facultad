1a31bef1cdcf582aa3f5ffb5e7bb1ce35dd283fa
juanchisin <71896282+juanchii-coder@users.noreply.github.com>
2024-05-01 17:43:07 -0300

    xxxxx

--- 2024/.NET/Practica/proyectosDotnet/teoria7/Almacen/Almacen.Aplicacion/obj/Debug/net8.0/Almacen.Aplicacion.AssemblyInfo.cs
+++ 2024/.NET/Practica/proyectosDotnet/teoria7/Almacen/Almacen.Aplicacion/obj/Debug/net8.0/Almacen.Aplicacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Almacen.Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d7db859d16367f3b53b31415386a8bd9ae788f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caf375989e7746c90c97a71a40eb414902f5cf85")]
 [assembly: System.Reflection.AssemblyProductAttribute("Almacen.Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Almacen.Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
